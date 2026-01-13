34f21b0bd8b1d59cbb0da2ee60de01339cedc561
aswinijena9937-ctrl <aswinijena9937@gmail.com>
2026-01-13 15:35:39 +0530

    Update service worker to use new cache version

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,38 @@
+const CACHE_NAME = "donkey-cache-v2"; // 🔁 CHANGE THIS EVERY UPDATE
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+// Install
 self.addEventListener("install", event => {
+  self.skipWaiting(); // force new SW to activate
   event.waitUntil(
-    caches.open("donkey-cache").then(cache => {
-      return cache.addAll([
-        "index.html",
-        "manifest.json",
-        "icon-192.png",
-        "icon-512.png"
-      ]);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
 });
 
+// Activate (delete old caches)
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
